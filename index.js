285a5da7ce554877d873f190fcc362425c795c2f
JumaWBalunywa <145361506+JumaWBalunywa@users.noreply.github.com>
2024-02-08 00:28:23 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function validUser(email, password){
+
+    const studentEmail = "yourstudentemail@mubs.ac.ug"
+    const studentPassword = "yourstudentnumber@2024"
+
+    if (email === studentEmail && password === studentPassword)
+    {console.log ("USE")}
+    else{
+        console.log ("INVALID CREDENTIALS")
+    }
+
+
+
+}
+validUser("yourstudentemail@mubs.ac.ug", "yourstudentnumber@2024");
